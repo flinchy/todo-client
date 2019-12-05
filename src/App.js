31954ef4c43cf51b02d3b862c1f32d4cf87eb09a
Chisom <iwowochisom@gmail.com>
2019-12-05 20:08:21 +0100

    Add AlertState in App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Home from './components/pages/Home';
+import Alert from './components/layout/Alert';
 import NotFound from './components/pages/NotFound';
+
+import AlertState from './context/alert/AlertState';
 
 function App() {
   return (
-    <Router>
-      {/* Navbar goes here */}
-      <div className="container">
-        <div className="card todo_area">
-          <div className="todo_content">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route component={NotFound} />
-            </Switch>
+    <AlertState>
+      <Router>
+        {/* Navbar goes here */}
+        <div className="container">
+        <Alert />
+          <div className="card todo_area">
+            <div className="todo_content">
+
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
           </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </AlertState>
   );
 }
 
