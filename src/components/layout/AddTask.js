e17dbeed8b3862ab0ec5e3456331d23ff4ebb8e5
Chisom <iwowochisom@gmail.com>
2019-12-05 18:09:48 +0100

    Add onchange handler for AddTask component

--- src/components/layout/AddTask.js
+++ src/components/layout/AddTask.js
@@ -1,22 +1,37 @@
-
-import React from 'react'
+import React, { useState } from 'react';
 
 const AddTask = () => {
+  const [todo, setTodo] = useState('');
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(todo);
+    setTodo('');
+  };
+
+  const onChange = e => {
+    setTodo(e.target.value);
+  };
+
   return (
     <div>
-      <form className="form">
-          <input
+      <form className="form" onSubmit={onSubmit}>
+        <input
           type="text"
           name="todo"
-          value=""
+          value={todo}
           placeholder="Add a Task.."
           className="bg-dark todo_input"
-
-          />
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="addTodo"
+          className="btn btn-light btn-dark"
+        />
       </form>
     </div>
-  )
-}
+  );
+};
 
-
-export default AddTask
+export default AddTask;
