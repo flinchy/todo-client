9351a9f174e0ed7a795522f8608e7447163246aa
Chisom <iwowochisom@gmail.com>
2019-12-05 20:07:40 +0100

    Create Alert.js Component to display Alert

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import AlertContext from '../../context/alert/AlertState';
+import AlertContext from '../../context/alert/alertContext';
 
 const Alert = () => {
   const alertContext = useContext(AlertContext);
