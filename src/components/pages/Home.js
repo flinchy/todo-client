08070dc742b96c960bdad75b75a01972b6553580
Chisom <iwowochisom@gmail.com>
2019-12-05 17:41:42 +0100

    Add Home Component

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import AddTask from '../layout/AddTask';
+
+const Home = () => {
+  return (
+    <Fragment>
+        <AddTask />
+    </Fragment>
+  )
+}
+
+export default Home
