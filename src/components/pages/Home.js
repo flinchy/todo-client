b5b40b977b89cf8d39b86d0a94c8da56c2545cbf
Chisom <iwowochisom@gmail.com>
2019-12-08 16:44:55 +0100

    Create Hook to load All todo on page load

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import AddTask from '../layout/AddTask';
 import Todo from '../layout/Todo';
+import TodoContext from '../../context/todo/todoContext';
 
 const Home = () => {
+  const todoContext = useContext(TodoContext);
+
+  useEffect(() => {
+    todoContext.getAllTodos();
+     // eslint-disable-next-line
+  }, []);
+
   return (
     <Fragment>
-        <AddTask />
-        <Todo />
+      <AddTask />
+      <Todo />
     </Fragment>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
