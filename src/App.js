f80dd2c3bc326ccaf9c4d61f32ca3320b2b87ffd
Chisom <iwowochisom@gmail.com>
2019-12-08 16:36:59 +0100

    Add TodoState to App.js

--- src/App.js
+++ src/App.js
@@ -6,27 +6,29 @@ import Home from './components/pages/Home';
 import Alert from './components/layout/Alert';
 import NotFound from './components/pages/NotFound';
 
+import TodoState from './context/todo/TodoState';
 import AlertState from './context/alert/AlertState';
 
 function App() {
   return (
-    <AlertState>
-      <Router>
-        {/* Navbar goes here */}
-        <div className="container">
-        <Alert />
-          <div className="card todo_area">
-            <div className="todo_content">
-
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route component={NotFound} />
-              </Switch>
+    <TodoState>
+      <AlertState>
+        <Router>
+          {/* Navbar goes here */}
+          <div className="container">
+            <Alert />
+            <div className="card todo_area">
+              <div className="todo_content">
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route component={NotFound} />
+                </Switch>
+              </div>
             </div>
           </div>
-        </div>
-      </Router>
-    </AlertState>
+        </Router>
+      </AlertState>
+    </TodoState>
   );
 }
 
