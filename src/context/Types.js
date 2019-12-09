94ef5b13943388c319bc54f14a35b092db1b7479
Chisom <iwowochisom@gmail.com>
2019-12-09 11:15:55 +0100

    Add type to create a todo

--- src/context/Types.js
+++ src/context/Types.js
@@ -1,4 +1,5 @@
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 export const GET_TODOS = "GET_TODOS";
+export const CREATE_TODO = "CREATE_TODO";
 export const SET_LOADING = "SET_LOADING";
