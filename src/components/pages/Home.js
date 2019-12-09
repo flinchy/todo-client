3272ad6fca76fe0e0d2dd28268fc4a58d43d8e62
Chisom <iwowochisom@gmail.com>
2019-12-09 11:11:58 +0100

    Pass prop todo to Todo Component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,28 @@
 import React, { Fragment, useContext, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import AddTask from '../layout/AddTask';
 import Todo from '../layout/Todo';
 import TodoContext from '../../context/todo/todoContext';
 
 const Home = () => {
   const todoContext = useContext(TodoContext);
+  const { todos } = todoContext;
 
   useEffect(() => {
     todoContext.getAllTodos();
-     // eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
 
   return (
     <Fragment>
       <AddTask />
-      <Todo />
+      <TransitionGroup>
+        {todos.map(todo => (
+          <CSSTransition key={todo.id} timeout={500}>
+            <Todo todo={todo} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
