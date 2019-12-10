f7d812f86127efe24983289916466110efbc30a0
Chisom <iwowochisom@gmail.com>
2019-12-10 13:59:34 +0100

    Add Types to update todo

--- src/context/Types.js
+++ src/context/Types.js
@@ -2,4 +2,7 @@ export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 export const GET_TODOS = "GET_TODOS";
 export const CREATE_TODO = "CREATE_TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+export const SET_CURRENT = "SET_CURRENT";
+export const CLEAR_CURRENT = "CLEAR_CURRENT";
 export const SET_LOADING = "SET_LOADING";
